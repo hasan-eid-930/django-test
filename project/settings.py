6110b1995525de86159ca32d5648890c3d7899d3
Hasan Eid <hasaneid930920@gmail.com>
2023-01-19 12:01:22 +0300

    alter ALLOW_LIST

--- project/settings.py
+++ project/settings.py
@@ -26,9 +26,9 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['web-production-0ba2.up.railway.app','127.0.0.1']
+ALLOWED_HOSTS = ['web-production-3ea6.up.railway.app','127.0.0.1']
 
-CSRF_TRUSTED_ORIGINS = ['https://web-production-0ba2.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-3ea6.up.railway.app/']
 
 
 # Application definition
